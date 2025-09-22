6e8b0fd03411eaf74a6cde8f0efcb0a46285219d
sarra2108 <ssass058@uottawa.ca>
2025-09-21 22:06:17 -0400

    :((

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,13 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 import path from 'path'
 // https://vite.dev/config/
-export default defineConfig(({ command }) => ({
+export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: command === "build" ? "/portfolio-website/" : "/",
+  base: "/",
 
 resolve:{
   alias:{
     "@": path.resolve(path.dirname(new URL(import.meta.url).pathname), './src'),
   },
 }
-}));
+});
